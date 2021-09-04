6717943ccdfdcc87b69ddf5550177326e5b26108
gadgetfather <kamblea635@gmail.com>
2021-09-04 09:56:49 +0530

    button clicked added

---
+++ app.js
@@ -0,0 +1,10 @@
+var btnTranslate = document.querySelector("#btn-translate");
+
+
+btnTranslate.addEventListener("click",clickHandler)
+
+function clickHandler(){
+    console.log("Clicked")
+    
+}
+
